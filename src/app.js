1e684183f446050c01aaa7ddcd3b173c78cda439
mizuki.kamei <mizuki.k20030811@gmail.com>
2025-08-04 00:13:27 +0900

    こんにちは

--- src/app.js
+++ src/app.js
@@ -1,7 +1,3 @@
 import AOS from 'aos';
 import 'aos/dist/aos.css';
-AOS.init({
-  duration: 800, // アニメーションが完了するまでの時間（ミリ秒）
-  once: false, // falseに設定すると、スクロールのたびにアニメーションが再生される
-  offset: 100, // 画面の100px手前でアニメーションを開始
-});
+AOS.init();
